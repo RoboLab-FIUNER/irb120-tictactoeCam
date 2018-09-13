f758bcf8830082bedc4b81d62285f345e9fda48c
Juan Cruz Gassó Loncan <juancruzgassoloncan@gmail.com>
2018-09-13 18:54:26 -0300

    add lisence to helper.py

--- helper.py
+++ helper.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep  6 10:00:15 2018
+ Copyright (C) 2018 FI-UNER Robotic Group
 
-@author: juan
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@mail: robotica@ingenieria.uner.edu.ar
 """
 import cv2
 import numpy as np
