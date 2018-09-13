e6cb951837aa74661671c4888474494751168c79
Juan Cruz Gassó Loncan <juancruzgassoloncan@gmail.com>
2018-09-13 18:55:38 -0300

    add lisence detec_board.py

--- detec_board.py
+++ detec_board.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep  5 11:59:10 2018
-
-@author: juan
+ Copyright (C) 2018 FI-UNER Robotic Group
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@mail: robotica@ingenieria.uner.edu.ar
 """
 import cv2
 import numpy as np
